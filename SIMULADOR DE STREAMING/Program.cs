ae011017d21429814f25516aecbfcde15f479be8
CRISTIAN SEBASTIAN HERNANDEZ MACARIO <jaja36189@gmail.com>
2026-03-21 23:07:28 -0600

    Agregar archivos de proyecto.

---
+++ SIMULADOR DE STREAMING/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido a la plataforma de Streaming");
+
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("1. Evaluar contenido");
+            Console.WriteLine("2. Ver reglas");
+            Console.WriteLine("3. Ver estadisticas");
+            Console.WriteLine("4. Reiniciar estadisticas");
+            Console.WriteLine("5. Salir");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Evaluando contenido...");
+                    break;
+                case 2:
+                    Console.WriteLine("Mostrando reglas...");
+                    break;
+                case 3:
+                    Console.WriteLine("Mostrando estadisticas...");
+                    break;
+                case 4:
+                    Console.WriteLine("Reiniciando estadisticas...");
+                    break;
+                case 5:
+                    Console.WriteLine("Hasta pronto!");
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+
+        } while (opcion != 5);
+    }
+}
